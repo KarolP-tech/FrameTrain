61341cdd4d37881756d5233edf68e547e9cdb592
KarolP-tech <karol.paschek@ferienwohnung-zornheim.de>
2025-11-17 10:34:10 +0100

    Route.ts prisma fixes

--- website/src/lib/prisma.ts
+++ website/src/lib/prisma.ts
@@ -4,6 +4,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'], // wichtig: verhindert lazy client caching
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
